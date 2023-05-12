2db5dd058d1c2b876cca7a1e2576a2f3f3f5e280
Anthony Hung Vu <hvu24@horizon.csueastbay.edu>
2023-05-12 00:44:18 -0700

    home page animation

--- react-app/src/components/Footer/footer.js
+++ react-app/src/components/Footer/footer.js
@@ -3,8 +3,8 @@ import './footer.css';
 
 function Footer() {
     return (
-        <footer>
-            <div style={{ display: 'flex', justifyContent: 'space-around', backgroundColor: '#6c757d' }}>
+        <footer style={{zIndex:'999', position: 'relative'}}>
+            <div style={{ display: 'flex', justifyContent: 'space-around', backgroundColor: '#6c757d', zIndex: '999' }}>
                 <a href='https://www.linkedin.com/in/anthony-v-67a490214/' className='footer-link' ><i class="fa-brands fa-linkedin"></i></a>
                 <a href='https://github.com/hvu24' className='footer-link' ><i class="fa-brands fa-github"></i></a>
             </div>
