0e0fc0577d598d892a2e8a93e639f1142c843783
Anthony Hung Vu <hvu24@horizon.csueastbay.edu>
2024-03-09 19:40:34 -0800

    made footer links open on new page and more visible

--- react-app/src/components/Footer/footer.js
+++ react-app/src/components/Footer/footer.js
@@ -3,10 +3,14 @@ import './footer.css';
 
 function Footer() {
     return (
-        <footer style={{zIndex:'999', position: 'relative'}}>
+        <footer style={{ zIndex: '999', position: 'relative' }}>
             <div style={{ display: 'flex', justifyContent: 'space-around', backgroundColor: '#6c757d', zIndex: '999' }}>
-                <a href='https://www.linkedin.com/in/anthony-v-67a490214/' className='footer-link' ><i class="fa-brands fa-linkedin"></i></a>
-                <a href='https://github.com/hvu24' className='footer-link' ><i class="fa-brands fa-github"></i></a>
+                <a href='https://www.linkedin.com/in/anthony-v-67a490214/' target='_blank' className='footer-link'>
+                    <i class="fa-brands fa-linkedin" style={{ fontSize: '24px' }}></i>
+                </a>
+                <a href='https://github.com/hvu24' target='_blank' className='footer-link'>
+                    <i class="fa-brands fa-github" style={{ fontSize: '24px' }}></i>
+                </a>
             </div>
         </footer>
 
